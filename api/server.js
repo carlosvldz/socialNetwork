26085237e4f2a5b62fd5b545d530f0308a64eb7a
carlosvldz <me@carlosvldz.com>
2020-09-04 00:53:55 -0500

    first commit

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const swaggerUi = require('swagger-ui-express');
+
+const config = require('../config');
+const user = require('./components/user/network');
+const auth = require('./components/auth/network');
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+const swaggerDoc = require('./swagger.json');
+
+//Router
+app.use('/api/user', user);
+app.use('/api/auth', auth);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+app.listen(config.api.port, () => {
+    console.log('API listen in port ', config.api.port);
+});
