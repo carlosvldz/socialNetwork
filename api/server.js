f0c161a6bc087521634b7519510829fa3feb54d3
carlosvldz <me@carlosvldz.com>
2020-09-06 18:38:32 -0500

    add post and store remote service

--- api/server.js
+++ api/server.js
@@ -5,8 +5,7 @@ const swaggerUi = require('swagger-ui-express');
 const config = require('../config');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
-const post = require('./components/post/network');
-const error = require('../network/errors');
+const errors = require('../network/errors');
 
 const app = express()
 
@@ -14,12 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 const swaggerDoc = require('./swagger.json');
-const errors = require('../network/errors');
 
 //Router
 app.use('/api/user', user);
 app.use('/api/auth', auth);
-app.use('/api/post', post);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(errors); 
--- config.js
+++ config.js
@@ -2,6 +2,9 @@ module.exports = {
     api: {
         port: process.env.API_PORT || 3000,
     },
+    post: {
+        port: process.env.POST_PORT || 3002,
+    },
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecret',
     },
@@ -11,4 +14,8 @@ module.exports = {
         password: process.env.MYSQL_PASS || 'WTV41XU29p',
         database: process.env.MYSQL_DB || '0GCeXVTIi6',
     },
+    mysqlService: {
+        host: process.env.MYSQL_SRV_HOST || 'localhost',
+        port: process.env.MYSQL_SRV_PORT || 3001,
+    }
 }
